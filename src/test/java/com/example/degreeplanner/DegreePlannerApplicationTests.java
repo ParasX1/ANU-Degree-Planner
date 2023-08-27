4b6b85c71386571748f4a8a8708714643a8840d8
Khennane <khennaneprogramming@gmail.com>
2023-08-27 23:17:31 +1000

    pushing spring initialized files

---
+++ src/test/java/com/example/degreeplanner/DegreePlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.degreeplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DegreePlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
